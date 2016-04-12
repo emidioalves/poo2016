52c99fbb4a07386fdeeab695451596823c20215c
Emidio Alves <emidio_alves@hotmail.com>
2016-04-11 22:54:57 -0300

    Resolucao Exercicio 1 Cap3

---
+++ Cap3/src/exercicio1/BalancoTrimestral.java
@@ -0,0 +1,16 @@
+package exercicio1;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+		
+		System.out.println ("Gastos do Trimestre: " + gastosTrimestre);
+	}
+
+}
